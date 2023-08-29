3c9a0e893d33f514fa64864a0cf3e3df827baf57
itszoney <141705573+itszoney@users.noreply.github.com>
2023-08-29 11:56:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
  #Broadcast Manager Module - Copyright (c) 2023 itszoney
 # This module handles broadcasting messages to subscribed users.
 import config
-from QuizBot import quiz_bot
+from QuizBot import QuizBot
 
 if __name__ == "__main__":
     quiz_bot.app.run()
