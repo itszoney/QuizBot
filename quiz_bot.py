ce8e58c1c841536e2d5e98f5f124e6dde8395d8c
itszoney <141705573+itszoney@users.noreply.github.com>
2023-08-29 17:16:58 +0530

    Update quiz_bot.py

--- quiz_bot.py
+++ quiz_bot.py
@@ -1,5 +1,6 @@
 # Broadcast Manager Module - Copyright (c) 2023 itszoney
 # This module handles broadcasting messages to subscribed users.
+from QuizBot.quiz_bot import QuizBot
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
 import config
