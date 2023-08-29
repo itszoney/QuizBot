658169ae69a0ad9785d7ad8762a4a346fb2f14a8
itszoney <141705573+itszoney@users.noreply.github.com>
2023-08-29 11:43:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
-API_ID = "your_api_id"
+API_ID = 21309577
 
-API_HASH = "your_api_hash"
+API_HASH = "df2554b54a9eb9e572979b5db2d0cc79"
 
-BOT_TOKEN = "your_bot_token"
+BOT_TOKEN = "6195545238:AAFdru2wsVwQyKzWsXv2Ltl5YDKIViDX-x4"
 
-MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb+srv://ghoda:loda@cluster0.fgrpret.mongodb.net/?retryWrites=true&
+w=majority"
 
-MONGO_DB_NAME = "your_database_name"
+MONGO_DB_NAME = "zoney"
 
 MONGO_USERS_COLLECTION = "users"
 
 MONGO_CHATS_COLLECTION = "chats"
 
-BOT_OWNER_ID = your_bot_owner_id
+BOT_OWNER_ID = 6417401336
 
-LOGGER_GROUP_ID = your_logger_group_id
+LOGGER_GROUP_ID = -1001603189739
 
 SESSION_NAME = "your_bot_session"
 
