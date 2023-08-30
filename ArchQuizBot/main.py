a29d748760ddfc888a13175a44856ee793c4be99
itszoney <141705573+itszoney@users.noreply.github.com>
2023-08-30 21:40:32 +0530

    Update main.py

--- ArchQuizBot/main.py
+++ ArchQuizBot/main.py
@@ -1,6 +1,9 @@
  #Broadcast Manager Module - Copyright (c) 2023 itszoney
 # This module handles broadcasting messages to subscribed users.
-from QuizBot.quiz_bot import QuizBot
+from pyrogram import idle
+from ArchQuizBot import QuizBot
+from AsuX.QuizBot import ALL_QuizBot
+#from QuizBot.quiz_bot import QuizBot
 import config
 from QuizBot import QuizBot
 
