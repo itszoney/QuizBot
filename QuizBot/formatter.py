d438f04e0f93793aac85574f5a65e8562a813fa6
itszoney <Michaelcjohnson216@gmail.com>
2023-08-30 21:29:10 +0530

    ___ɴᴏᴏʙs

--- QuizBot/formatter.py
+++
@@ -1,7 +0,0 @@
-class Formatter:
-    @staticmethod
-    def format_quiz_question(question, options):
-        formatted_question = f"Q: {question}\n\n"
-        for i, option in enumerate(options, start=1):
-            formatted_question += f"{i}. {option}\n"
-        return formatted_question
